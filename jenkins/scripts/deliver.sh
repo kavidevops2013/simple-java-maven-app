a65d6f134320451f4c83c61b28736eec65ead25a
kavidevops2013 <kavinayaviolinist2013@gmail.com>
2024-10-29 22:05:20 +0530

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -5,23 +5,27 @@ echo 'into the local Maven repository, which will ultimately be stored in'
 echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
 echo 'volume).'
 set -x
-mvn jar:jar install:install help:evaluate -Dexpression=project.name
+mvn clean install -DskipTests
 set +x
 
 echo 'The following command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 set -x
-NAME=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.name | sed 's/[^a-zA-Z0-9._-]//g')
+NAME=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.name)
 set +x
 
 echo 'The following command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
-VERSION=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.version | sed 's/[^a-zA-Z0-9._-]//g')
+VERSION=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.version)
 set +x
 
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
+
+# Remove any non-standard characters from NAME and VERSION
+NAME=$(echo "$NAME" | sed 's/[^a-zA-Z0-9._-]//g')
+VERSION=$(echo "$VERSION" | sed 's/[^a-zA-Z0-9._-]//g')
 
 # Check if the jar file exists before attempting to run it
 JAR_FILE="target/${NAME}-${VERSION}.jar"
@@ -31,5 +35,5 @@ if [[ -f "$JAR_FILE" ]]; then
     set +x
 else
     echo "Error: The jar file '$JAR_FILE' does not exist."
-    exit 1
+    exit 1
 fi
